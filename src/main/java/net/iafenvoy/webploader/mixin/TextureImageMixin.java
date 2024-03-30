db0226f1be159c5b6fe29a16c6166e657f8e7cca
IAFEnvoy <tanksheng545@126.com>
2024-03-30 10:19:41 +0800

    change inject

--- src/main/java/net/iafenvoy/webploader/mixin/TextureImageMixin.java
+++ src/main/java/net/iafenvoy/webploader/mixin/TextureImageMixin.java
@@ -1,37 +1,50 @@
 package net.iafenvoy.webploader.mixin;
 
-import com.mojang.blaze3d.platform.NativeImage;
 import net.burningtnt.webp.SimpleWEBPLoader;
 import net.burningtnt.webp.utils.RGBABuffer;
 import net.iafenvoy.webploader.WebpLoader;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 
 //Use class path string here since TextureImage is a protected class.
 @Mixin(targets = {"net.minecraft.client.renderer.texture.SimpleTexture$TextureImage"})
 public class TextureImageMixin {
-    @Redirect(method = "load", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/mojang/blaze3d/platform/NativeImage;read(Ljava/io/InputStream;)Lcom/mojang/blaze3d/platform/NativeImage;"))
-    private static NativeImage handleLoad(ResourceManager resourceManager, ResourceLocation resourceLocation) {
+    @ModifyArg(method = "load", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;read(Ljava/io/InputStream;)Lcom/mojang/blaze3d/platform/NativeImage;"), index = 0)
+    private static InputStream handleLoad(InputStream inputStream) {
+        ByteArrayOutputStream clonedBytes = webpLoader$cloneInputStream(inputStream);
+        if (clonedBytes == null) return inputStream;
+        InputStream stream1 = new ByteArrayInputStream(clonedBytes.toByteArray());
+        InputStream stream2 = new ByteArrayInputStream(clonedBytes.toByteArray());
         try {
-            String resourcePath = resourceLocation.getPath();
-            Resource resource = resourceManager.getResource(resourceLocation);
-            if (resourcePath.endsWith(".webp")) {
-                WebpLoader.LOGGER.info("Loading " + resourceLocation);
-                InputStream inputStream = resource.getInputStream();
-                RGBABuffer.AbsoluteRGBABuffer rgbaBuffer = SimpleWEBPLoader.decode(inputStream);
-                return NativeImage.read(ByteBuffer.wrap(rgbaBuffer.getRGBAData()));
-            }
-            return NativeImage.read(resource.getInputStream());
+            RGBABuffer.AbsoluteRGBABuffer webpBuffer = SimpleWEBPLoader.decode(stream1);
+            return new ByteArrayInputStream(webpBuffer.getRGBAData());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            WebpLoader.LOGGER.debug(e.getMessage());
+            return stream2;
+        }
+    }
+
+    @Unique
+    private static ByteArrayOutputStream webpLoader$cloneInputStream(InputStream input) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = input.read(buffer)) > -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.flush();
+            return baos;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
